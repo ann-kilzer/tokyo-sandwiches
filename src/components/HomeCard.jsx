7ab414c10050ec4cb81994bf6bccbe7e3416e7ef
Ann 杏 Kilzer <akilzer@gmail.com>
2023-02-22 21:21:22 +0900

    sandwich shop

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,64 +1,40 @@
-import { useState } from 'react'
-import Button from '@mui/material/Button'
-import Card from '@mui/material/Card'
-import Divider from '@mui/material/Divider'
-import Stack from '@mui/material/Stack'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
-import reactLogo from '../assets/react.svg'
 import './HomeCard.css'
+import Grid from '@mui/material/Grid'
+import SandwichShop from './SandwichShop'
 
 export default function HomeCard() {
-  const [count, setCount] = useState(0)
+  const SandwichShops = [
+
+    { name: "Harry's Sandwich Company", image: 'harrys-sandwich.jpg', description: 'Friendly San Francisco style sandwich shop and bar in Harajuku' },
+    { name: 'Bar Triad & Bar Trench', image: 'triad-trench.jpg', description: 'Craft cocktails and a great Tonkatsu sandwich' },
+    { name: 'Ivy Place', image: 'ivy-place.jpg', description: 'Relaxed patio and indoor dining ad Daikanyama T-Site' },
+    { name: 'Freeman Shokudo', image: 'freeman-shokudo.jpg', description: 'Cool American BBQ restaurant' },
+    { name: 'Haikara Fried Chicken', image: 'haikara.jpg', description: 'Chicken Sandwiches in Akihabara' },
+  ]
+
+  const renderShops = SandwichShops.map((shop) => (
+    <Grid item key={shop.name}>
+      <SandwichShop
+        name={shop.name}
+        image={shop.image}
+        description={shop.description}
+      />
+    </Grid>
+  ))
 
   return (
-    <Card>
-      <Stack
-        spacing={2}
-        justifyContent="center"
-        alignItems="center"
-        paddingY={4}
-      >
-        <Stack direction="row">
-          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-            <img src="/sample-react-app/vite.svg" className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </Stack>
-        <Typography variant="h1">Vite + React</Typography>
-        <Button
-          variant="contained"
-          sx={{ width: '150px' }}
-          onClick={() => setCount((counter) => counter + 1)}
-        >
-          count is
-          {' '}
-          {count}
-        </Button>
+    <Container sx={{ flexGrow: 1 }}>
+      <Typography variant="h3" my={2}>Best Sandwiches in Tokyo</Typography>
 
-        <Typography variant="body1">
-          Edit
-          {' '}
-          <code>src/App.jsx</code>
-          {' '}
-          and save to test HMR
-        </Typography>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2} columns={{ xs: 4, sm: 8, md: 12 }} mt={4}>
+          {renderShops}
+        </Grid>
+      </Box>
 
-        <Typography variant="body2">
-          Click on the Vite and React logos to learn more
-        </Typography>
-
-        <Divider />
-
-        <Typography variant="h2">
-          GitHub Pages
-        </Typography>
-
-        <Typography variant="body1">
-          This page is deployed on GitHub Pages
-        </Typography>
-      </Stack>
-    </Card>
+    </Container>
   )
 }
